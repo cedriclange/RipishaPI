551bc4d7d57ab24344fccc19c2dbdba93d23772e
Unknown <langemataso@gmail.com>
2017-10-20 16:58:19 +0100

    starting point library for mobile and web app

---
+++ csharp/source/Ripisha/Ripisha.Mobile/Configurations/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ripisha.Mobile.Configurations
+{
+    public class Configuration
+    {
+        private static Configuration singleton;
+        private Dictionary<string, string> configs = new Dictionary<string, string>();
+
+        //this is where we will edit all our configuration to be used into our app
+
+    }
+}
---
+++ csharp/source/Ripisha/Ripisha.Mobile/Models/Ripisha.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ripisha.Mobile.Models
+{
+    //this class is the object model 
+    //to be mapped in the database
+    public class Ripisha
+    {
+        public int Id { get; set; }
+        public int Type { get; set; }
+        public string Receipt { get; set; }
+        public DateTime Time { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public int Status { get; set; }
+        public long Amount { get; set; }
+        public long PostBalance { get; set; }
+        public string Note { get; set; }
+    }
+}
